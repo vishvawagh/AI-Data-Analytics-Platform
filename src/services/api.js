71ebf5d2150f563841b9341e5773639b2607ec9e
Vishvachaitanya Wagh <98593551+vishvawagh@users.noreply.github.com>
2026-04-19 11:57:24 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://ai-data-analytics-be.onrender.com",
   withCredentials: true   // 🔥 MUST BE TRUE
 });
 
